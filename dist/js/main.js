00edacbacd2e6b7bed06d130cc296a99f9a2282b
80mahd08 <amarimahdi8@gmail.com>
2023-03-14 00:05:27 +0100

    add more

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,24 +1,22 @@
-let switchContainer = <HTMLSpanElement>  document.querySelector(".switchContainer");
-let switchElement = <HTMLSpanElement> document.querySelector('.switchContainer > *')
+"use strict";
+let switchContainer = document.querySelector(".switchContainer");
+let switchElement = document.querySelector('.switchContainer > *');
 switchElement.style.cssText = `
     left: 3px
-`
-
-let count = 1
-switchContainer.addEventListener("click" , ()=>{
-    count++
+`;
+let count = 1;
+switchContainer.addEventListener("click", () => {
+    count++;
     if (count % 2 == 0) {
         console.log("hello mom");
         switchElement.style.cssText = `
             right: 3px
-        `
+        `;
     }
     else {
         console.log("hello dad");
         switchElement.style.cssText = `
             left: 3px
-        `
+        `;
     }
-
-})
-
+});
