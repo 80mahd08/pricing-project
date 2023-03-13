95f43c02f9bf46679efdab85089f8d9c4573888e
80mahd08 <amarimahdi8@gmail.com>
2023-03-14 00:49:12 +0100

    add js

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,7 @@
 "use strict";
 let switchContainer = document.querySelector(".switchContainer");
 let switchElement = document.querySelector('.switchContainer > *');
+let h2Card = document.querySelectorAll("h2");
 switchElement.style.cssText = `
     left: 3px
 `;
@@ -8,15 +9,25 @@ let count = 1;
 switchContainer.addEventListener("click", () => {
     count++;
     if (count % 2 == 0) {
-        console.log("hello mom");
         switchElement.style.cssText = `
             right: 3px
         `;
+        h2Card.forEach((ele) => {
+            ele.innerHTML = `$${addNine(ele.innerHTML.split('').filter(el => el != "$").join(''))}`;
+        });
     }
     else {
-        console.log("hello dad");
         switchElement.style.cssText = `
             left: 3px
         `;
+        h2Card.forEach((ele) => {
+            ele.innerHTML = `$${removeNine(ele.innerHTML.split('').filter(el => el != "$").join(''))}`;
+        });
     }
 });
+function addNine(str) {
+    return `${str.slice(0, str.indexOf('.'))}9${str.slice(str.indexOf('.'), str.length)}`;
+}
+function removeNine(str) {
+    return `${str.slice(0, str.indexOf('.') - 1)}${str.slice(str.indexOf('.'), str.length)}`;
+}
